19bd647cbd1fb93503e81b6487bf399d99b30423
Damian Krawczyk <damian.krawczyk@limberduck.org>
2022-05-13 16:33:51 +0200

    drop python 3.6

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
