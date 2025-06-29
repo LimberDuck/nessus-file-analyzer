3768081eb6e46babb080d35bf2062e89e8c37419
Damian Krawczyk <damian@damiankrawczyk.com>
2025-06-29 20:40:17 +0200

    p3.11 test drop

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
